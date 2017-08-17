10d04b888229d958a2ec1fea0e80d8405da00a23
FrancoHermelo <fran_hermelo@hotmail.com>
2017-08-17 19:45:51 -0300

    Creacion de tablas solicitudes, clientes, orden_trabajo

---
+++ database/migrations/2017_08_17_210247_create_solicitudes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSolicitudesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */ 
+    public function up()
+    {
+        Schema::create('solicitudes', function (Blueprint $table) {
+            $table->increments('id_solicitudes');
+            $table->integer('id_cliente');
+            $table->integer('id_usuario');
+            $table->string('titulo');
+            $table->string('descripcion');
+            $table->enum('estado', ['En espera', 'Atendida']);
+            $table->datetime('fecha');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('solicitudes');
+    }
+}
