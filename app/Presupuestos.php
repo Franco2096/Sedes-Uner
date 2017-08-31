1a3efd241b2abf3d9b909414c330cb246a7272b7
Matias Sack <matisk.90@gmail.com>
2017-08-31 18:59:10 -0300

    Se agrega AMB Falta Solicitud para probar complemente
    Se agrega amb presupuesto, se edita el mensaje seccion para que aparezca abajo de la barra de navegacion y el color verde.

--- app/Presupuestos.php
+++ app/Presupuestos.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Presupuestos extends Model
 {
     protected $table = 'presupuestos';
-    protected $fillable =['descripcion','estado','solicitud_id','fecha_emision'];
+    protected $fillable =['descripcion','estado','solicitud_id','fecha_emision','usuario_id'];
 
 
-    
+
 }
---
+++ resources/views/presupuesto/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.appMenu')
+
+@section('Abajobarra')
+		@if(Session::has('message'))
+			<div class="alert alert-success alert-dismissible" role="alert">
+		  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		  {{ Session::get('message') }}
+		 </div>
+		@endif
+@endsection
+
+
+@section('tabla')
+	<style >
+		.nuevo{
+			width: 200px;
+		}
+	</style>
+  <a href="/presupuestos/create" class="btn btn-success nuevo" >Nuevo</a>
+	<table class="table">
+		<thead>
+
+			<th>Descripcion</th>
+			<th>Estado</th>
+			<th>Fecha de emision</th>
+			<th>Solicitud Asociada</th>
+			<th>Usuario asociado</th>
+			<th>Operaciones</th>
+			<th></th>
+		</thead>
+		@foreach($presupuestos as $presupuesto)
+		<tbody>
+			<td>{{ $presupuesto->descripcion }}</td>
+			<td>{{ $presupuesto->estado }}</td>
+			<td>{{ $presupuesto->fecha_emision }}</td>
+			<td>{{ $presupuesto->solicitud_id }}</td>
+			<td>{{ $presupuesto->usuario_id }}</td>
+
+
+
+			<td>{!! link_to_route('presupuestos.edit', $title = 'Editar', $parameters = $presupuesto->id, $attributes = ['class' =>'btn btn-primary' ]) !!}</td>
+			<td>{!! Form::open(['route' =>['presupuestos.destroy', $presupuesto->id], 'method' => 'DELETE']) !!}
+					{!! Form::submit('Eliminar',['class' => 'btn btn-danger']) !!}
+					{!! Form::close() !!}
+			</td>
+
+
+
+
+		</tbody>
+		@endforeach
+	</table>
+
+	{!! $presupuestos->render() !!}
+
+@stop
